a0e017853e7aea324715e437fc5dbf325af6cc7a
Tazmah <abdulmuizzayo6@gmail.com>
2023-02-10 01:52:17 +0100

    updated the profile

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ const hiddenActivity = document.querySelectorAll(".activity__hidden")
 
 for (let i = 0; i < moreData.length; i++) {
     moreData[i].addEventListener("click", () => {
-        console.log(hiddenActivity[i].style.display);
         if (hiddenActivity[i].style.display == "none" || hiddenActivity[i].style.display == '') {
-            console.log(`click ${i}`);
             hiddenActivity[i].style.display = "flex"
             dataICon[i].innerHTML = "&#8593;"
         } else {
@@ -26,3 +24,56 @@ for (let i = 0; i < moreData.length; i++) {
         }
     })
 }
+
+
+let oldPassword = document.getElementById("oldPasswordValue");
+let newPassword = document.getElementById("newPasswordValue");
+let showNewPassword = document.querySelector(".showNewPassword");
+let showOldPassword = document.querySelector(".showOldPassword");
+
+
+showOldPassword.addEventListener("click", () => {
+    if (oldPassword.type === "password") {
+        oldPassword.type = "text"
+    } else {
+        oldPassword.type = "password"
+    }
+})
+
+showNewPassword.addEventListener("click", () => {
+    if (newPassword.type === "password") {
+        newPassword.type = "text"
+    } else {
+        newPassword.type = "password"
+    }
+})
+
+
+const passwordSection = document.getElementById("password")
+const transactionSection = document.getElementById("transaction")
+
+const transactionInfo = document.querySelector(".transactionInfo")
+const passwordInfo = document.querySelector(".passwordInfo");
+
+
+// transactionSection.addEventListener("click", () => {
+//     if (transactionInfo.style.display == "none" || transactionInfo.style.display == '') {
+//         transactionInfo.style.display == "flex"
+//         passwordInfo.style.display == "none"
+//         console.log("hi");
+//     } else {
+//         transactionInfo.style.display == "none"
+//         passwordInfo.style.display == "flex"
+//         console.log("hhhhi");
+//     }
+// })
+// passwordSection.addEventListener("click", () => {
+//     if (passwordInfo.style.display == "none" || passwordInfo.style.display == '') {
+//         transactionInfo.style.display == "none"
+//         passwordInfo.style.display == "flex"
+
+//     } else {
+//         transactionInfo.style.display == "flex"
+//         passwordInfo.style.display == "none"
+//     }
+// })
